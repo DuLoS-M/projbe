22c77ade088420d8ceb6e66c185181c1b4ee1f54
DuLoS-M <e_craud@hotmail.com>
2024-08-24 09:53:03 -0600

    fixes

--- src/main/java/com/example/projbe/entity/Users.java
+++ src/main/java/com/example/projbe/entity/Users.java
@@ -10,8 +10,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,10 +37,9 @@ public class Users {
     @Column(name = "registration_date", nullable = false, updatable = false)
     private LocalDateTime registrationDate;
 
-    @Enumerated(EnumType.STRING)
+    // @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
-
 
     @PrePersist
     protected void onCreate() {
